fc7b7fc192124d8c6937cb1aa59405f4b37c63bc
mike_ <mikezhifailiangzhihui@my.jcu.edu.au>
2023-12-03 17:05:59 +0800

    change some answers

--- prac_04/list_warmup.py
+++ prac_04/list_warmup.py
@@ -11,9 +11,9 @@
 9)numbers + [6, 5, 3]
 
 Ans:
-Q1)The value of numbers[0] is 3.
-Q2)The value of numbers[-1] is 2.
-Q3)The value of numbers[3] is 1.
+Q1)The value of numbers[0] = 3.
+Q2)The value of numbers[-1] = 2.
+Q3)The value of numbers[3] = 1.
 Q4)The value of numbers[:-1] is [3, 1, 4, 1, 5, 9]. This is a slice of the numbers list that includes all elements except the last one.
 Q5)The value of numbers[3:4] is [1]. This is a slice of the numbers list that includes the element at index 3, but excludes the element at index 4.
 Q6)The expression is 5 in numbers. This will return True if 5 is in the list, else false.
