3bd9685ef27fb9da8ea63eefa6a2b4771f5ccf15
mike_ <mikezhifailiangzhihui@my.jcu.edu.au>
2023-12-03 16:52:08 +0800

    finished list warm up

---
+++ prac_04/list_warmup.py
@@ -0,0 +1,23 @@
+"""numbers = [3, 1, 4, 1, 5, 9, 2]
+
+1.numbers[0]
+2.numbers[-1]
+3.numbers[3]
+4.numbers[:-1]
+5.numbers[3:4]
+6)5 in numbers
+7)7 in numbers
+8)"3" in numbers
+9)numbers + [6, 5, 3]
+
+Ans:
+Q1)The value of numbers[0] is 3.
+Q2)The value of numbers[-1] is 2.
+Q3)The value of numbers[3] is 1.
+Q4)The value of numbers[:-1] is [3, 1, 4, 1, 5, 9]. This is a slice of the numbers list that includes all elements except the last one.
+Q5)The value of numbers[3:4] is [1]. This is a slice of the numbers list that includes the element at index 3, but excludes the element at index 4.
+Q6)The expression is 5 in numbers. This will return True if 5 is in the list, else false.
+Q7)The expression is 7 in numbers. This will return False as 7 is not in the list.
+Q8)The expression is "3" in numbers. This will return False because "3" is not in the list.
+Q9)The value of numbers + [6, 5, 3] is [3, 1, 4, 1, 5, 9, 2, 6, 5, 3].
+This is a new list that contains the elements of numbers followed by the elements [6, 5, 3]."""
